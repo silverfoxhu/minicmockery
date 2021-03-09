88ab2bdefee92fcfeb200dc1f1f3ee41d26cfb70
silverfox <297095993@qq.com>
2021-03-09 22:57:42 +0800

    实现了申请内存的越界访问的故障报出

--- assert_test.c
+++ assert_test.c
@@ -1,7 +1,17 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
+#include <malloc.h>
 #include "minicmockery.h"
+
+extern void *_test_malloc(const size_t size, const char *file, const int line);
+extern void *_test_calloc(const size_t number_of_elements, const size_t size,
+                          const char *file, const int line);
+extern void _test_free(void *const ptr, const char *file, const int line);
+
+#define malloc(size) _test_malloc(size, __FILE__, __LINE__)
+#define calloc(num, size) _test_calloc(num, size, __FILE__, __LINE__)
+#define free(ptr) _test_free(ptr, __FILE__, __LINE__)
 
 int add(int a, int b)
 {
@@ -11,6 +21,26 @@ int add(int a, int b)
 int sub(int a, int b)
 {
     return a - b;
+}
+
+void leak_memory()
+{
+    int *const temporary = (int *)malloc(sizeof(int));
+    *temporary = 0;
+}
+
+void buffer_overflow()
+{
+    char *const memory = (char *)malloc(sizeof(int));
+    memory[sizeof(int)] = '!';
+    free(memory);
+}
+
+void buffer_underflow()
+{
+    char *const memory = (char *)malloc(sizeof(int));
+    memory[-1] = '!';
+    free(memory);
 }
 
 void test_add(void **state)
@@ -27,11 +57,31 @@ void test_sub(void **state)
     assert_int_not_equal(sub(2, 1), 0);
 }
 
+void leak_memory_test(void **state)
+{
+    leak_memory();
+}
+
+// Test case that fails as buffer_overflow() corrupts an allocated block.
+void buffer_overflow_test(void **state)
+{
+    buffer_overflow();
+}
+
+// Test case that fails as buffer_underflow() corrupts an allocated block.
+void buffer_underflow_test(void **state)
+{
+    buffer_underflow();
+}
+
 int main(int argc, char *argv[])
 {
     const UnitTest tests[] = {
         unit_test(test_add),
         unit_test(test_sub),
+        unit_test(leak_memory_test),
+        unit_test(buffer_overflow_test),
+        unit_test(buffer_underflow_test),
     };
     return run_tests(tests);
 }
--- minicmockery.c
+++ minicmockery.c
@@ -2,11 +2,53 @@
 #include <setjmp.h>
 #include "minicmockery.h"
 #include <bits/types.h>
+#include <malloc.h>
+#include <string.h>
+
+// Size of guard bytes around dynamically allocated blocks.
+#define MALLOC_GUARD_SIZE 16
+// Pattern used to initialize guard blocks.
+#define MALLOC_GUARD_PATTERN 0xEF
+// Alignment of allocated blocks.  NOTE: This must be base2.
+#define MALLOC_ALIGNMENT sizeof(size_t)
+// Pattern used to initialize memory allocated with test_malloc().
+#define MALLOC_ALLOC_PATTERN 0xBA
+#define MALLOC_FREE_PATTERN 0xCD
+
+// Calculates the number of elements in an array.
+#define ARRAY_LENGTH(x) (sizeof(x) / sizeof((x)[0]))
+
+typedef struct ListNode
+{
+    const void *value;
+    struct ListNode *next;
+    struct ListNode *prev;
+} ListNode;
+
+// Location within some source code.
+typedef struct SourceLocation
+{
+    const char *file;
+    int line;
+} SourceLocation;
+
+// Debug information for malloc().
+typedef struct MallocBlockInfo
+{
+    void *block;             // Address of the block returned by malloc().
+    size_t allocated_size;   // Total size of the allocated block.
+    size_t size;             // Request block size.
+    SourceLocation location; // Where the block was allocated.
+    ListNode node;           // Node within list of all allocated blocks.
+} MallocBlockInfo;
 
 typedef struct
 {
     jmp_buf run_test_env;
     __uint32_t failed_count;
+
+    // List of all currently allocated blocks.
+    ListNode allocated_blocks;
 } minicmockery_entity_t;
 static minicmockery_entity_t minicmockery_entity;
 
@@ -50,6 +92,117 @@ void _assert_int_not_equal(const int a, const int b, const char *const file,
         _fail(file, line);
     }
 }
+
+// Initialize a list node.
+static ListNode *list_initialize(ListNode *const node)
+{
+    node->next = node;
+    node->prev = node;
+    return node;
+}
+
+// Add new_node to the end of the list.
+static ListNode *list_add(ListNode *const head, ListNode *new_node)
+{
+    new_node->next = head;
+    new_node->prev = head->prev;
+    head->prev->next = new_node;
+    head->prev = new_node;
+    return new_node;
+}
+
+// Remove a node from a list.
+static ListNode *list_remove(ListNode *const node)
+{
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+
+    return node;
+}
+
+/* Remove a list node from a list and free the node. */
+/*
+static void list_remove_free(ListNode *const node)
+{
+    free(list_remove(node));
+}
+*/
+
+static ListNode *get_allocated_blocks_list()
+{
+    if (NULL == minicmockery_entity.allocated_blocks.value)
+    {
+        list_initialize(&minicmockery_entity.allocated_blocks);
+        minicmockery_entity.allocated_blocks.value = (void *)1;
+    }
+
+    return &minicmockery_entity.allocated_blocks;
+}
+
+// Set a source location.
+static void set_source_location(SourceLocation *const location, const char *const file, const int line)
+{
+    location->file = file;
+    location->line = line;
+}
+
+void *_test_malloc(const size_t size, const char *file, const int line)
+{
+    MallocBlockInfo *block_info;
+    ListNode *const block_list = get_allocated_blocks_list();
+    const size_t allocate_size = size + (MALLOC_GUARD_SIZE * 2) + sizeof(*block_info) + MALLOC_ALIGNMENT;
+
+    char *const block = (char *)malloc(allocate_size);
+
+    // Calculate the returned address.
+    char *ptr = (char *)(((size_t)block + MALLOC_GUARD_SIZE + sizeof(*block_info) +
+                          MALLOC_ALIGNMENT) &
+                         ~(MALLOC_ALIGNMENT - 1));
+
+    // Initialize the guard blocks.
+    memset(ptr - MALLOC_GUARD_SIZE, MALLOC_GUARD_PATTERN, MALLOC_GUARD_SIZE);
+    memset(ptr + size, MALLOC_GUARD_PATTERN, MALLOC_GUARD_SIZE);
+    memset(ptr, MALLOC_ALLOC_PATTERN, size);
+
+    block_info = (MallocBlockInfo *)(ptr - (MALLOC_GUARD_SIZE + sizeof(*block_info)));
+    set_source_location(&block_info->location, file, line);
+    block_info->allocated_size = allocate_size;
+    block_info->size = size;
+    block_info->block = block;
+    block_info->node.value = block_info;
+    list_add(block_list, &block_info->node);
+    return ptr;
+}
+
+void _test_free(void *const ptr, const char *file, const int line)
+{
+    unsigned int i;
+    char *block = (char *)ptr;
+    MallocBlockInfo *block_info;
+    block_info = (MallocBlockInfo *)(block - (MALLOC_GUARD_SIZE + sizeof(*block_info)));
+    char *guards[2] = {block - MALLOC_GUARD_SIZE, block + block_info->size};
+    for (i = 0; i < ARRAY_LENGTH(guards); i++)
+    {
+        unsigned int j;
+        char *const guard = guards[i];
+        for (j = 0; j < MALLOC_GUARD_SIZE; j++)
+        {
+            if (MALLOC_GUARD_PATTERN != guard[j])
+            {
+                printf("Guard block of 0x%08lx size=%ld allocated by %s:%d at 0x%08lx is corrupt\n",
+                       (size_t)ptr, block_info->size,
+                       block_info->location.file, block_info->location.line,
+                       (size_t)&guard[j]);
+                _fail(file, line);
+            }
+        }
+    }
+    list_remove(&block_info->node);
+    block = block_info->block;
+    memset(block, MALLOC_FREE_PATTERN, block_info->allocated_size);
+    free(block);
+}
+
 int _run_tests(const UnitTest *const tests, const size_t number_of_tests)
 {
     __uint32_t index = 0;
@@ -64,6 +217,7 @@ int _run_tests(const UnitTest *const tests, const size_t number_of_tests)
         if (0 == ret)
         {
             test->function(&state);
+
             printf("%s: test completely successed\n", test->name);
         }
         else
